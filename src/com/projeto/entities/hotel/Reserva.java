603285d3f44ff4149b066e2da3aab5871b21ec45
vini-21samuel <130195075+vini-21samuel@users.noreply.github.com>
2024-09-22 01:33:29 -0300

    classes finalizadas

---
+++ src/com/projeto/entities/hotel/Reserva.java
@@ -0,0 +1,42 @@
+package com.projeto.entities.hotel;
+
+import java.util.Date;
+
+public class Reserva {
+    private int codigoReserva;
+    private Hospede hospede;
+    private Quarto quarto;
+    private Date dataEntrada;
+    private Date dataSaida;
+    private double valorTotal;
+
+    public Reserva() {
+    }
+
+    public Reserva(int codigoReserva, Hospede hospede, Quarto quarto, Date dataEntrada, Date dataSaida) {
+        this.codigoReserva = codigoReserva;
+        this.hospede = hospede;
+        this.quarto = quarto;
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+        calcularValorTotal();
+    }
+
+    // Métodos getters e setters
+
+    private void calcularValorTotal() {
+        if (dataEntrada != null && dataSaida != null && quarto != null) {
+            long dias = (dataSaida.getTime() - dataEntrada.getTime()) / (1000 * 60 * 60 * 24);
+            this.valorTotal = dias * quarto.getPrecoDiaria();
+        }
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Código da Reserva: " + codigoReserva);
+        hospede.exibirInformacoes();
+        quarto.exibirInformacoesQuarto();
+        System.out.println("Data de Entrada: " + dataEntrada);
+        System.out.println("Data de Saída: " + dataSaida);
+        System.out.println("Valor Total: " + valorTotal);
+    }
+}
